9b1b52880ceae48a93eadf7a03dd418e93cd7cae
Nikhil <144448080+nikhilsharma82@users.noreply.github.com>
2025-07-28 18:14:08 +0530

    initial commit with tailwind config

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+      <p className="text-3xl font-bold underline bg-blue-500 text-white p-4 rounded">
+        Hello Vite + React + Tailwind CSS!
+      </p>
+
+      </div>
+       
+    </>
+  )
+}
+
+export default App
